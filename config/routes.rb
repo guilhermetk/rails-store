e8169813e9bd0029de9645f859c34c82358b3ef5
Guilherme Tiscoski <github@guilhermetiscoski.com>
2025-10-06 11:54:10 -0500

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ resources :products do
   resources :subscribers, only: [ :create ]
 end
 
+resource :unsubscribe, only: [ :show ]
+
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
